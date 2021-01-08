b6cf72e2626fb0789dca98ac74e26894b9f03725
Muhammad Ivan Riansyah Putra <ivanriansyah.putra@gmail.com>
2021-01-08 15:16:12 +0700

    Done Lt 1

--- Assets/Scripts/SwitchScene.cs
+++ Assets/Scripts/SwitchScene.cs
@@ -6,9 +6,14 @@ using UnityEngine.SceneManagement;
 public class SwitchScene : MonoBehaviour
 {
     Scene currentScene;
+    public GameObject infoUI;
+    FirstPersonAIO playerMovement;
+    Transform player;
 
     void Start()
     {
+        player = GameObject.FindWithTag("Player").transform;
+        playerMovement = player.GetComponent<FirstPersonAIO>();
         currentScene = SceneManager.GetActiveScene();
     }
 
@@ -18,5 +23,40 @@ public class SwitchScene : MonoBehaviour
         {
             SceneManager.LoadScene(currentScene.buildIndex + 1);
         }
+
+        if (other.gameObject.tag == "LiftSwitch")
+        {
+            infoUI.gameObject.SetActive(true);
+            playerMovement.enableCameraMovement = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "LiftSwitch")
+        {
+            infoUI.gameObject.SetActive(false);
+            playerMovement.enableCameraMovement = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    public void ChangeScene(int id)
+    {
+        StartCoroutine(OpenScene(id));
+    }
+
+     private IEnumerator OpenScene(int id)
+    {
+        if (id < 0)
+        {
+            yield break;
+        }
+        
+        SceneManager.LoadScene(id);
     }
 }
